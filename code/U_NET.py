f888c557adb0562f7ef335576b6b648eeaa3f709
Pedro Costa <pedro.vendascosta@gmail.com>
2017-11-17 09:57:51 +0000

    U-Net is predicting the transmission map. The color balanced image is then fed to the classifier!

--- code/U_NET.py
+++ code/U_NET.py
@@ -2,23 +2,30 @@ import numpy as np
 import os
 import re
 from scipy import ndimage, misc
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Input
+from keras.optimizers import Adam
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Input, Lambda
 from keras.preprocessing.image import ImageDataGenerator, array_to_img
 from keras.layers.merge import Concatenate
 from keras.models import Model
+from deep_preprocess.classifier import Classifier
 
 from keras import backend as K
 
-train_data_dir = "/Volumes/Karnik/data/train/normal"
+train_data_dir = "D:\\work\\datasets\\dr\\Messidor\\mess\\train"
+val_data_dir = "D:\\work\\datasets\\dr\\Messidor\\mess\\val"
+test_data_dir = "D:\\work\\datasets\\dr\\Messidor\\mess\\test"
+
+"""train_data_dir = "/Volumes/Karnik/data/train/normal"
 val_data_dir = "/Volumes/Karnik/data/val/normal"
 test_data_dir = "/Volumes/Karnik/data/test/normal"
-
-train_data_patho = "/Volumes/Karnik/data/train/pathological"
+"""
+"""train_data_patho = "/Volumes/Karnik/data/train/pathological"
 val_data_patho = "/Volumes/Karnik/data/val/pathological"
 test_data_patho = "/Volumes/Karnik/data/test/pathological"
+"""
 
 
-images = []
+"""images = []
 for root, dirnames, filenames in os.walk("/Volumes/Karnik/data/test/normal/images"):
     for filename in filenames:
         if re.search("\.(jpg|jpeg|png|bmp|tiff)$", filename):
@@ -26,7 +33,7 @@ for root, dirnames, filenames in os.walk("/Volumes/Karnik/data/test/normal/image
             image = ndimage.imread(filepath, mode="RGB")
             image_resized = misc.imresize(image, (512, 512))
             images.append(image_resized)
-im=np.asarray(images)
+im = np.asarray(images)
 
 images_patho = []
 for root, dirnames, filenames in os.walk("/Volumes/Karnik/data/test/pathological"):
@@ -36,13 +43,13 @@ for root, dirnames, filenames in os.walk("/Volumes/Karnik/data/test/pathological
             image = ndimage.imread(filepath, mode="RGB")
             image_resized = misc.imresize(image, (512, 512))
             images_patho.append(image_resized)
-im_patho=np.asarray(images_patho)
+im_patho = np.asarray(images_patho)"""
 
 def u_net():
     nrow = 512
     ncol = 512
     nch = 3
-    
+
     if K.image_data_format() == 'channels_first':
         axis = 1
         input_img = Input(shape=(nch, nrow, ncol))
@@ -50,55 +57,61 @@ def u_net():
         axis = -1
         input_img = Input(shape=(nrow, ncol, nch))
 
-    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(input_img)
-    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(conv1)
+    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(input_img)
+    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(pool1)
-    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(conv2)
+    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same")(pool1)
+    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same")(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    
-    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(pool2)
-    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    
-    conv4 = Conv2D(256, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(pool3)
+
+    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same")(pool2)
+    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same")(conv3)
+    # pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    """conv4 = Conv2D(256, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(pool3)
     conv4 = Conv2D(256, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
-    
+
     conv5 = Conv2D(512, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(pool4)
     conv5 = Conv2D(512, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(conv5)
-    
+
     up6 = Concatenate(axis=axis)([Conv2D(256, (2, 2), activation="relu", padding="same", kernel_initializer="uniform")(UpSampling2D(size=(2, 2))(conv5)), conv4])
     conv6 = Conv2D(256, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(up6)
     conv6 = Conv2D(256, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(conv6)
-    
+
     up7 = Concatenate(axis=axis)([Conv2D(128, (2, 2), activation="relu", padding="same", kernel_initializer="uniform")(UpSampling2D(size=(2, 2))(conv6)), conv3])
     conv7 = Conv2D(128, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(up7)
-    conv7 = Conv2D(128, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(conv7)
-    
-    up8 = Concatenate(axis=axis)([Conv2D(64, (2, 2), activation="relu", padding="same", kernel_initializer="uniform")(UpSampling2D(size=(2, 2))(conv7)), conv2])
-    conv8 = Conv2D(64, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(up8)
-    conv8 = Conv2D(64, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(conv8)
-    
-    up9 = Concatenate(axis=axis)([Conv2D(32, (2, 2), activation="relu", padding="same", kernel_initializer="uniform")(UpSampling2D(size=(2, 2))(conv8)), conv1])
-    conv9 = Conv2D(32, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(up9)
-    conv9 = Conv2D(32, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(conv9)
-    
-    conv10 = Conv2D(1, (1, 1), activation="sigmoid", padding="same", kernel_initializer="uniform")(conv9)
-    
-    model = Model(inputs=input_img, outputs=conv10)
-    
-    model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
-    
+    conv7 = Conv2D(128, (3, 3), activation="relu", padding="same", kernel_initializer="uniform")(conv7)"""
+
+    up8 = Concatenate(axis=axis)([Conv2D(64, (2, 2), activation="relu", padding="same")(UpSampling2D(size=(2, 2))(conv3)), conv2])
+    conv8 = Conv2D(64, (3, 3), activation="relu", padding="same")(up8)
+    conv8 = Conv2D(64, (3, 3), activation="relu", padding="same")(conv8)
+
+    up9 = Concatenate(axis=axis)([Conv2D(32, (2, 2), activation="relu", padding="same")(UpSampling2D(size=(2, 2))(conv8)), conv1])
+    conv9 = Conv2D(32, (3, 3), activation="relu", padding="same")(up9)
+    conv9 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv9)
+
+    t = Conv2D(1, (1, 1), activation="sigmoid", padding="same")(conv9)
+
+    def color_balance(t):
+        # Icb = (I - A(1 - t)) / t
+        t_rep = K.repeat_elements(t, 3, axis)
+        return (input_img - (1 - t_rep)) / (t_rep + K.epsilon())
+
+    img_cb = Lambda(color_balance)(t)
+    img_cb_norm = Lambda(lambda x: (x * 255) - 127)(img_cb)
+
+    classifier = Classifier()
+    for layer in classifier.base_model.layers:
+        layer.trainable = False
+
+    model = Model(inputs=input_img, outputs=classifier.model(img_cb_norm))
+
+    opt = Adam(lr=1e-3)
+    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+
     return model
-
-
-def auxiliar_generator(gen):
-    """This generator should be used to test the unet implementation only."""
-    while True:
-        x, _ = next(gen)
-        yield x, np.mean(x, axis=-1, keepdims=True)
 
 
 if __name__ == '__main__':
@@ -108,44 +121,25 @@ if __name__ == '__main__':
     target_ch = 3
     b_size = 4
     e = 20
-    
+
     train_data = ImageDataGenerator(rescale=1./255)
     val_data = ImageDataGenerator(rescale=1. / 255)
     test_data = ImageDataGenerator(rescale=1. / 255)
-    
+
     train_gen = train_data.flow_from_directory(train_data_dir, target_size=(target_width, target_height), batch_size=b_size, class_mode='binary')
     val_gen = val_data.flow_from_directory(val_data_dir, target_size=(target_width, target_height), batch_size=b_size, class_mode='binary')
     test_gen = test_data.flow_from_directory(test_data_dir, target_size=(target_width, target_height), batch_size=b_size, class_mode='binary')
 
-    train_gen_patho = train_data.flow_from_directory(train_data_dir, target_size=(target_width, target_height),batch_size=b_size, class_mode='binary')
-    val_gen_patho = val_data.flow_from_directory(val_data_dir, target_size=(target_width, target_height), batch_size=b_size,class_mode='binary')
-    test_gen_patho = test_data.flow_from_directory(test_data_dir, target_size=(target_width, target_height),batch_size=b_size, class_mode='binary')
-
     model = u_net()
-    
-    x, y = next(train_gen)
-    xp, yp = next(train_gen_patho)
-    
+
     num_train = train_gen.n
     num_val = val_gen.n
     num_test = test_gen.n
 
-    num_train_patho = train_gen_patho.n
-    num_val_patho = val_gen_patho.n
-    num_test_patho = test_gen_patho.n
-
     print("Fitting the model...")
-    aux_train_gen = auxiliar_generator(train_gen)
-    aux_val_gen = auxiliar_generator(val_gen)
-    aux_test_gen = auxiliar_generator(test_gen)
 
-    aux_train_gen_p = auxiliar_generator(train_gen_patho)
-    aux_val_gen_p = auxiliar_generator(val_gen_patho)
-    aux_test_gen_p = auxiliar_generator(test_gen_patho)
+    model.fit_generator(generator=train_gen, steps_per_epoch=num_train//b_size, epochs=e, validation_data=val_gen, validation_steps=num_val)
 
-    model.fit_generator(generator=aux_train_gen, steps_per_epoch=num_train//b_size, epochs=e, validation_data=aux_val_gen, validation_steps=num_val)
-    model.fit_generator(generator=aux_train_gen_p, steps_per_epoch=num_train_patho // b_size, epochs=e,validation_data=aux_val_gen_p, validation_steps=num_val_patho)
-    
     print("Predicting from the model...")
 
     test_res = model.predict(im, batch_size=4, verbose=1)
@@ -153,7 +147,7 @@ if __name__ == '__main__':
 
     np.save('/Volumes/Karnik/test_res.npy', test_res)
     np.save('/Volumes/Karnik/test_res_patho.npy', test_res_patho)
-    
+
     print("Converting to image")
     load_array = np.load('/Volumes/Karnik/test_res.npy')
     for l in range(load_array.shape[0]):
--- code/deep_preprocess/classifier.py
+++ code/deep_preprocess/classifier.py
@@ -4,7 +4,7 @@ from keras.optimizers import Adam
 from keras.layers.core import Dropout
 from keras.layers import GlobalAveragePooling2D
 from keras.layers.advanced_activations import LeakyReLU
-from utils.exceptions import InvalidArchitectureException
+from deep_preprocess.utils.exceptions import InvalidArchitectureException
 from keras.applications.vgg16 import VGG16, preprocess_input as preprocess_input_vgg16
 from keras.applications.vgg19 import VGG19, preprocess_input as preprocess_input_vgg19
 from keras.applications.resnet50 import ResNet50, preprocess_input as preprocess_input_resnet
@@ -14,20 +14,20 @@ from keras.applications.inception_v3 import InceptionV3, preprocess_input as pre
 class Classifier:
     """Class that wraps a CNN classifier."""
 
-    def __init__(self, architecture='inception', n_features=2048, weights='imagenet'):
+    def __init__(self, architecture='inception', n_features=1024, weights='imagenet', input_tensor=None):
         self.architecture = architecture
         self.n_features = n_features
 
         m, preprocessing_fn = self._get_architecture(architecture)
-        base_model = m(weights=weights, include_top=False)
+        self.base_model = m(weights=weights, include_top=False, input_tensor=input_tensor)
 
-        x = GlobalAveragePooling2D()(base_model.output)
+        x = GlobalAveragePooling2D()(self.base_model.output)
         x = Dense(n_features)(x)
         x = LeakyReLU(0.02)(x)
         x = Dropout(0.5)(x)
         out = Dense(1, activation='sigmoid')(x)
 
-        self.model = Model(input=base_model.input, output=out)
+        self.model = Model(input=self.base_model.input, output=out)
 
     def prepare_to_init(self, init_lr):
         """Set transfered layers untrainable and compile model."""
