990d4d0d219281313210408d4ead7df363cca4ec
Pedro Costa <pedro.vendascosta@gmail.com>
2017-11-24 16:23:44 +0000

    Improved code to allow for the finetuning of the inception network.

--- code/U_NET.py
+++ code/U_NET.py
@@ -10,60 +10,71 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img
 
 from keras import backend as K
 
-def u_net():
-    nrow = 512
-    ncol = 512
-    nch = 3
 
-    if K.image_data_format() == 'channels_first':
-        axis = 1
-        input_img = Input(shape=(nch, nrow, ncol))
-    else:
-        axis = -1
-        input_img = Input(shape=(nrow, ncol, nch))
+class DeepPreprocess:
 
-    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(input_img)
-    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    def __init__(self, nrow=512, ncol=512, nch=3):
+        if K.image_data_format() == 'channels_first':
+            axis = 1
+            input_img = Input(shape=(nch, nrow, ncol))
+        else:
+            axis = -1
+            input_img = Input(shape=(nrow, ncol, nch))
 
-    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same")(pool1)
-    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same")(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+        nf = 16
+        conv1 = Conv2D(nf, (3, 3), activation="relu", padding="same")(input_img)
+        conv1 = Conv2D(nf, (3, 3), activation="relu", padding="same")(conv1)
+        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same")(pool2)
-    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same")(conv3)
+        conv2 = Conv2D(nf*2, (3, 3), activation="relu", padding="same")(pool1)
+        conv2 = Conv2D(nf*2, (3, 3), activation="relu", padding="same")(conv2)
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    up8 = UpSampling2D(size=(2, 2))(conv3)
-    up8 = Concatenate(axis=axis)([Conv2D(64, (2, 2), activation="relu", padding="same")(up8), conv2])
-    conv8 = Conv2D(64, (3, 3), activation="relu", padding="same")(up8)
-    conv8 = Conv2D(64, (3, 3), activation="relu", padding="same")(conv8)
+        conv3 = Conv2D(nf*4, (3, 3), activation="relu", padding="same")(pool2)
+        conv3 = Conv2D(nf*4, (3, 3), activation="relu", padding="same")(conv3)
 
-    up9 = UpSampling2D(size=(2, 2))(conv8)
-    up9 = Concatenate(axis=axis)([Conv2D(32, (2, 2), activation="relu", padding="same")(up9), conv1])
-    conv9 = Conv2D(32, (3, 3), activation="relu", padding="same")(up9)
-    conv9 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv9)
+        up8 = UpSampling2D(size=(2, 2))(conv3)
+        up8 = Concatenate(axis=axis)([Conv2D(nf*2, (2, 2), activation="relu", padding="same")(up8), conv2])
+        conv8 = Conv2D(nf*2, (3, 3), activation="relu", padding="same")(up8)
+        conv8 = Conv2D(nf*2, (3, 3), activation="relu", padding="same")(conv8)
 
-    t = Conv2D(1, (1, 1), activation="sigmoid", padding="same")(conv9)
+        up9 = UpSampling2D(size=(2, 2))(conv8)
+        up9 = Concatenate(axis=axis)([Conv2D(nf, (2, 2), activation="relu", padding="same")(up9), conv1])
+        conv9 = Conv2D(nf, (3, 3), activation="relu", padding="same")(up9)
+        conv9 = Conv2D(nf, (3, 3), activation="relu", padding="same")(conv9)
 
-    def color_balance(t):
-        t_rep = K.repeat_elements(t, 3, axis)
-        return 1 - (((1 - input_img) - (1 - t_rep)) / (t_rep + K.epsilon()))
+        t = Conv2D(1, (1, 1), activation="sigmoid", padding="same")(conv9)
 
-    img_cb = Lambda(color_balance)(t)
-    img_cb_norm = Lambda(lambda x: (x * 255) - 127)(img_cb)
+        def color_balance(t):
+            t_rep = K.repeat_elements(t, 3, axis)
+            return 1 - (((1 - input_img) - (1 - t_rep)) / (t_rep + K.epsilon()))
 
-    classifier = Classifier()
-    for layer in classifier.base_model.layers:
-        layer.trainable = False
+        img_cb = Lambda(color_balance)(t)
+        img_cb_norm = Lambda(lambda x: (x * 2) - 1)(img_cb)
 
-    model = Model(input_img, classifier.model(img_cb_norm))
+        self.classifier = Classifier()
 
-    opt = Adam(lr=1e-3)
-    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
-    preprocess_model = Model(input_img, img_cb)
-    transmission_model = Model(input_img, t)
+        out = self.classifier.model(img_cb_norm)
+        self.model = Model(input_img, out)
+        self.preprocess_models = Model(input_img, [img_cb, t])
 
-    return model, preprocess_model, transmission_model
+    def prepare_to_init(self):
+        for layer in self.classifier.base_model.layers:
+            layer.trainable = False
+
+        opt = Adam(lr=2e-4)
+        self.model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+
+        return self.model
+
+    def prepare_to_finetune(self):
+        for layer in self.classifier.base_model.layers:
+            layer.trainable = True
+
+        opt = Adam(lr=2e-4)
+        self.model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+
+        return self.model
 
 
 def parse_params():
@@ -85,7 +96,7 @@ if __name__ == '__main__':
     args = parse_params()
     target_size = (512, 512)
     b_size = 4
-    e = 10
+    e = 6
 
     train_data = ImageDataGenerator(rescale=1./255)
     val_data = ImageDataGenerator(rescale=1./255)
@@ -99,26 +110,26 @@ if __name__ == '__main__':
     num_val = val_gen.n // b_size
     num_test = test_gen.n // b_size
 
-    model, preprocess_model, transmission_model = u_net()
+    unet = DeepPreprocess()
+    model = unet.prepare_to_init()
 
     print("Fitting the model...")
     model.fit_generator(generator=train_gen, steps_per_epoch=num_train, epochs=e, validation_data=val_gen, validation_steps=num_val, workers=8)
 
-    print("Predicting from the model...")
-    test_res = preprocess_model.predict_generator(test_gen, num_test, verbose=1)
-    test_t = transmission_model.predict_generator(test_gen, num_test, verbose=1)
+    print("Finetuning...")
+    # model = unet.prepare_to_finetune()
+    # model.fit_generator(generator=train_gen, steps_per_epoch=num_train, epochs=e, validation_data=val_gen, validation_steps=num_val, workers=8)
 
-    save_res_path = os.path.join(args.out_dir, 'test_res.npy')
-    save_t_path = os.path.join(args.out_dir, 'test_t.npy')
-    np.save(save_res_path, test_res)
-    np.save(save_t_path, test_t)
+    print("Predicting from the model...")
+    preprocess_models = unet.preprocess_models
+    test_res = preprocess_models.predict_generator(test_gen, num_test, verbose=1)
 
     print("Converting to image")
     for i, filename in enumerate(test_gen.filenames):
-        frame = test_res[i]
+        frame = test_res[0][i]
         frame = array_to_img(frame)
-        frame.save(os.path.join(args.out_dir, 'imgs', '{0}.jpg'.format(os.path.basename(filename))))
+        frame.save(os.path.join(args.out_dir, 'imgs', '{0}'.format(os.path.basename(filename))))
 
-        frame = test_t[i]
+        frame = test_res[1][i]
         frame = array_to_img(frame)
-        frame.save(os.path.join(args.out_dir, 't', '{0}.jpg'.format(os.path.basename(filename))))
+        frame.save(os.path.join(args.out_dir, 't', '{0}'.format(os.path.basename(filename))))
